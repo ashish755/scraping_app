e2e2bc7854aae1862a919324e1595c0b30d387a1
ashish616 <ashish.maharjan@coac.de>
2023-08-18 16:30:00 +0545

    added pdfplumber to extract text

--- src/apis/extraction_api.py
+++ src/apis/extraction_api.py
@@ -1,7 +1,57 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, UploadFile, File
+import pdfplumber
+import os
+import shutil
 
 router = APIRouter()
 
-@router.get("/parse_data_from_website")
-def parse_data():
-    pass
+upload_folder = "/tmp/documents"
+
+
+def create_file(filedata):
+    global upload_folder
+    file_object = filedata.file
+    # create empty file to copy the file_object to
+    if not os.path.exists(upload_folder):
+        os.mkdir(upload_folder)
+    path = os.path.join(upload_folder, filedata.filename)
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file_object, buffer)
+
+    return path
+
+@router.post("/get_number_of_pages")
+def parse_data(
+    filedata: UploadFile = File(...)
+):
+    file = create_file(filedata)
+    pdf = pdfplumber.open(file)
+
+    pages = pdf.pages[0]
+    total_pages = len(pdf.pages)
+    contents = pages.extract_words()
+
+    all_texts = []
+    for content in contents:
+        text = content["text"]
+        all_texts.append(text)
+
+    return {"Total number of pages": total_pages}
+
+@router.post("/get_pdf_contents")
+def parse_data(
+    filedata: UploadFile = File(...),
+    page_num: str = Query(None, description="")
+):
+    file = create_file(filedata)
+    pdf = pdfplumber.open(file)
+
+    pages = pdf.pages[int(page_num)-1]
+    contents = pages.extract_words()
+
+    all_texts = []
+    for content in contents:
+        text = content["text"]
+        all_texts.append(text)
+
+    return {"pdf contents": " ".join(all_texts)}
