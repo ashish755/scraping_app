2962addcb229703ed091a20f09d8aa0b3ef52f19
ashish755 <maharjanashish56@gmail.com>
2023-09-15 04:15:16 +0000

    changes headline

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 from src.apis.extraction_api import router as ExtractionApiRouter
 
 
-app = FastAPI(title="Web Scraping project")
+app = FastAPI(title="Data extraction project")
 app.include_router(ExtractionApiRouter)
